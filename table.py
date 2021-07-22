0fb9d4c870c5adcbcf8f8d5de3deaaf97ba5447f
neesh7 <76041032+neesh7@users.noreply.github.com>
2021-07-22 11:55:10 +0530

    Create table.py
    Simple program to create table

---
+++ table.py
@@ -0,0 +1,5 @@
+num = int(input("Enter the number of which you want to see the table))
+for i in range (1,num+1):
+                print("{}*{}={}".format(num,i,num*i)
+                      
+                      
